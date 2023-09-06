e421f0ef50249bfac4abdc5327823e7d6af1b5ca
Steven Maxwell <stevenmaxwell@Stephens-MacBook-Pro.local>
2023-09-06 15:09:18 -0400

    added client list

--- signal-server.js
+++ signal-server.js
@@ -6,15 +6,24 @@ const io = require('socket.io')(http, {
   }
 });
 
+const users = []
+
 app.get('/', (req, res) => {
   res.send('<h1>Hey Socket.io</h1>');
 });
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+  users.push(socket.id)
+  console.log("Number of active connections is now:", users.length)
 
   socket.on('disconnect', () => {
+    let index = users.indexOf(socket.id);
+    if (index > -1) {
+      users.splice(index, 1);
+    }
     console.log('user disconnected');
+    console.log("Number of active connections is now:", users.length)
   });
 
   socket.on('my message', (msg) => {
